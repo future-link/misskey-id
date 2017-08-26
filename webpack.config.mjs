225d875fc6a50087af2198a8e1766cc36325d70a
otofune <otofune@gmail.com>
2017-08-27 02:39:44 +0900

    webpack | productionかdevelopmentに固定する

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -14,7 +14,7 @@ export default {
         new webpack.DefinePlugin({
             process: {
                 env: {
-                    NODE_ENV: JSON.stringify(process.env.NODE_ENV ? process.env.NODE_ENV : 'development')
+                    NODE_ENV: JSON.stringify(process.env.NODE_ENV == 'production' ? process.env.NODE_ENV : 'development')
                 }
             }
         }),
